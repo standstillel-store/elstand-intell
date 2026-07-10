a1254187a38d826b45dadccee538635266432781
standstillel-store <standstillel@gmail.com>
2026-07-10 12:53:16 +0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'NOCTURN INTEL',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
