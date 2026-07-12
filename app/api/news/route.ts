e6392bae61770f8f93894d6672fba80c753630cf
standstillel-store <standstillel@gmail.com>
2026-07-12 11:52:16 +0700

    Delete app directory

--- app/api/news/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { getNews } from "@/lib/newsapi";
-
-export async function GET() {
-  const news = await getNews();
-  return NextResponse.json({ news });
-}
